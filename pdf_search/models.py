75624eca3bffb51a1f58646efa33ee0c1d010c6d
Yura Levchenko <levchenko.yurii024@gmail.com>
2024-03-17 18:43:21 +0200

    requirements.txt added

--- pdf_search/models.py
+++ pdf_search/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models.signals import post_delete
 
 
 class PDFDocument(models.Model):
--- pdf_search/tests.py
+++ pdf_search/tests.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 
 # Create your tests here.
 from django.test import TestCase
-from .models import PDFDocument
 from pdfminer.high_level import extract_text
 
 class PDFTextExtractionTestCase(TestCase):
